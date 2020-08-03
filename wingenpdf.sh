09c3423d3daca632303d5466059c387ee8bd5e3e
Oren Duer <oren@nvidia.com>
2020-08-03 13:53:06 +0300

    CSS for pandoc
    Signed-off-by: Oren Duer <oren@nvidia.com>

--- wingenpdf.sh
+++ wingenpdf.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-winpty docker run --rm -v "`pwd`":/github/workspace orendu/pandoc ///generate.sh md_cheatsheet.md md_cheatsheet css/github-typora2.css "MarkDown CheatSheet"
+
+winpty docker run --rm -v "`pwd`":/github/workspace orendu/pandoc //generate.sh md_cheatsheet.md md_cheatsheet css/archdoc.css "MarkDown CheatSheet"
+
+# Running pandoc directly (skip generate script to add ver,date and direct pass pandoc params)
+#winpty docker run --rm -v "`pwd`":/github/workspace -w //github/workspace orendu/pandoc pandoc --from gfm --to html5 --css css/archdoc.css -N --standalone -M title="MarkDown CheatSheet" -o md_cheatsheet.pdf md_cheatsheet.md
 

781c392d99731a36752f34894fce1a8af5b85be9
Oren Duer <oren@nvidia.com>
2020-08-25 15:18:10 +0300

    Using new pandoc container that supports multiple directories
    Signed-off-by: Oren Duer <oren@nvidia.com>

--- wingenpdf.sh
+++ wingenpdf.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-winpty docker run --rm -v "`pwd`":/github/workspace orendu/pandoc //generate.sh md_cheatsheet.md md_cheatsheet css/archdoc.css "MarkDown CheatSheet"
-
-# Running pandoc directly (skip generate script to add ver,date and direct pass pandoc params)
-#winpty docker run --rm -v "`pwd`":/github/workspace -w //github/workspace orendu/pandoc pandoc --from gfm --to html5 --css css/archdoc.css -N --standalone -M title="MarkDown CheatSheet" -o md_cheatsheet.pdf md_cheatsheet.md
-
+winpty docker run --rm -v "`pwd`":/github/workspace orendu/pandoc //generate.sh md_cheatsheet md_cheatsheet.md md_cheatsheet.pdf css/archdoc.css "MarkDown CheatSheet"
